c156a0c53d93af42ff6d9e02667e58222ad2c0c7
Aleksey Piskunov <piskunov.alesha@gmail.com>
2019-09-07 16:07:01 +0300

    PostgreSQL, SecretKey, Docker(-compose)

--- queif/views.py
+++ queif/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-# Create your views here.
+
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
